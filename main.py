eaae02ec8d009966172d2bf92b13c3377491d844
keller hill <hillkc@umich.edu>
2025-07-31 11:43:56 -0600

    Script for calculating P/L and sending message via twilio

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import json
 from dotenv import load_dotenv
 
 load_dotenv()
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
-TO_PHONE_NUMBER = os.getenv('TO_PHONE_NUMBER')
+TO_PHONE_NUMBER = os.getenv('TWILIO_TO')
 
 def load_portfolio(filename='portfolio.json'):
     with open(filename, 'r') as file:
